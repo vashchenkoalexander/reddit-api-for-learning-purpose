afa3867dda725b5468c80513db836612a4bb42de
Alex <asdw3a@gmail.com>
2023-06-14 23:35:28 +0300

    first commit

---
+++ src/main/java/com/example/redditbotjava17/RedditBotJava17Application.java
@@ -0,0 +1,13 @@
+package com.example.redditbotjava17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditBotJava17Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedditBotJava17Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/redditbotjava17/RedditBotJava17ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.redditbotjava17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditBotJava17ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
