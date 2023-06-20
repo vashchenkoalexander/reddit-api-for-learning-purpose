6596dcb4dc8bb270881cda4090046dc34e819dc7
Alex <asdw3a@gmail.com>
2023-06-20 11:01:07 +0300

    commit for working API

--- src/main/java/com/example/redditbotjava17/RedditBotJava17Application.java
+++ src/main/java/com/example/redditbotjava17/RedditBotJava17Application.java
@@ -2,12 +2,14 @@ package com.example.redditbotjava17;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import redditbot.RedditBotClass;
 
 @SpringBootApplication
 public class RedditBotJava17Application {
 
     public static void main(String[] args) {
         SpringApplication.run(RedditBotJava17Application.class, args);
+        RedditBotClass.main(args);
     }
 
 }
--- src/main/java/redditbot/RedditBotClass.java
+++ src/main/java/redditbot/RedditBotClass.java
@@ -11,6 +11,8 @@ import net.dean.jraw.oauth.OAuthHelper;
 import net.dean.jraw.http.UserAgent;
 import net.dean.jraw.pagination.DefaultPaginator;
 import net.dean.jraw.references.SubredditReference;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -21,9 +23,8 @@ import java.net.URL;
 import java.util.List;
 import java.util.Objects;
 
-
-public class redditbotclass {
-
+@Component
+public class RedditBotClass {
 
     private static final String YOUR_USERNAME = "Mysterious_Rewera";
     private static final String YOUR_PASSWORD = "Huf94SPUm5hfGCk";
@@ -53,7 +54,7 @@ public class redditbotclass {
             RedditClient reddit = OAuthHelper.automatic(adapter, credentials);
 
             // Specify the subreddit you want to connect to
-            // reddit for picks of dicks DickSlips southpark
+            // reddit for picks of southpark
             String subredditName = "southpark";
 
             // Create a DefaultPaginator for the subreddit
@@ -83,27 +84,29 @@ public class redditbotclass {
             for (Submission post : posts) {
 
                 // Check if the post is an image-based submission
-                if ((post.isSelfPost() || !Objects.equals(post.getPostHint(), "image")) & post.isNsfw()) {
-                    continue; // Skip non-image posts
-                }
+                if ((post.isSelfPost() || Objects.equals(post.getPostHint(), "image")) & post.isNsfw()) {
+                    // Retrieve the image URL from the post
+                    String imageUrl = post.getUrl();
 
-                // Retrieve the image URL from the post
-                String imageUrl = post.getUrl();
-
-                // Download the image and save it to a file
-                try (InputStream in = new URL(imageUrl).openStream()) {
-                    String fileName = post.getId() + ".jpg";
-                    FileOutputStream out = new FileOutputStream(new File ("D:\\Main\\RedditBot\\RedditBotJava17\\photos", fileName));
-                    byte[] buffer = new byte[4096];
-                    int bytesRead;
-                    while ((bytesRead = in.read(buffer)) != -1) {
-                        out.write(buffer, 0, bytesRead);
+                    // Download the image and save it to a file
+                    try (InputStream in = new URL(imageUrl).openStream()) {
+                        String fileName = post.getId() + ".jpg";
+                        FileOutputStream out = new FileOutputStream(new File ("D:\\Main\\RedditBot\\RedditBotJava17\\photos", fileName));
+                        byte[] buffer = new byte[4096];
+                        int bytesRead;
+                        while ((bytesRead = in.read(buffer)) != -1) {
+                            out.write(buffer, 0, bytesRead);
+                        }
+                        System.out.println("Downloaded image: " + fileName);
+                        out.close();
+                    } catch (IOException e) {
+                        System.err.println("Error downloading image: " + e.getMessage());
                     }
-                    System.out.println("Downloaded image: " + fileName);
-                    out.close();
-                } catch (IOException e) {
-                    System.err.println("Error downloading image: " + e.getMessage());
+                } else {
+                    System.out.println("None of images is compatible with condition");
                 }
+
+
 
             }
 
